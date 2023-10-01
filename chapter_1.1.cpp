caf2cf1d2ab949674dec77dbb7c189256fa1de22
Евгений <z1henia@gmail.com>
2023-10-01 17:45:41 +0300

    Разбор первой программы

---
+++ chapter_1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+    {
+        //изменение кодировки консоли://
+        system("chcp 1251>nul");
+        cout<<"Программируем на С++"<<endl;
+        system("pause>nul");
+        return 0;
+    }
